36ea1cae9985f0894b82fd40a7b2f9e085001a5c
David Spencer <rainsphere2k@gmail.com>
2026-01-27 19:37:24 -0500

    style: standardize quotes and formatting in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/80u_to_atlas/'
-    : '/'
-})
+  publicPath: process.env.NODE_ENV === "production" ? "/80u_to_atlas/" : "/",
+});
